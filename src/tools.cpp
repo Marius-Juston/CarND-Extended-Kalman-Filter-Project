559d988e280b601d0b11bab55fb9884a3e4fe78b
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-06 20:16:09 -0400

    created RSME function

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,8 +12,25 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-   * TODO: Calculate the RMSE here.
+   * Calculates the RMSE.
    */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+    if (estimations.empty() || estimations.size() != ground_truth.size()) {
+        std::cout << "The estimations are empty or the ground truth and estimations sizes are not equal" << std::endl;
+        return rmse;
+    }
+
+    for (unsigned int i = 0; i < estimations.size(); ++i) {
+        rmse += (VectorXd) (estimations[i] - ground_truth[i]).array().square();
+    }
+
+    rmse /= estimations.size();
+    rmse = rmse.array().sqrt();
+
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
