04894b423cd9ede019334d9d59c4d5b1e45984cc
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-06 20:59:38 -0400

    Added Jacobian Calculation code

--- src/tools.cpp
+++ src/tools.cpp
@@ -35,7 +35,32 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 
 MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
   /**
-   * TODO:
    * Calculate a Jacobian here.
    */
+
+  MatrixXd Hj(3, 4);
+  // recover state parameters
+  double px = x_state(0);
+  double py = x_state(1);
+  double vx = x_state(2);
+  double vy = x_state(3);
+
+  // check division by zero
+  if (px == 0 && py == 0) {
+    std::cout << "CalculateJacobian() - Error - Division by Zero" << std::endl;
+    return Hj;
+  }
+
+  // compute the Jacobian matrix
+  double distance = px * px + py * py;
+  double px_dist = px / sqrt(distance);
+  double py_dist = py / sqrt(distance);
+  double bottom_left = (vx * py - vy * px) / pow(distance, 3 / 2.);
+
+  Hj << px_dist, py_dist, 0, 0,
+      -py / distance, px / distance, 0, 0,
+      py * bottom_left, -px * bottom_left, px_dist, py_dist;
+
+  return Hj;
+
 }
